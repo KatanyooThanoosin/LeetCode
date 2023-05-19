93d76f87c33e742b81cd12ab65fcb37b0d350f5c
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2023-05-19 16:11:45 +0700

    Update _10_UNFINISHED_regular_expression_matching.py

--- _10_UNFINISHED_regular_expression_matching.py
+++ _10_UNFINISHED_regular_expression_matching.py
@@ -1,18 +1,22 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        while len(s)>0 and len(p)>0:
-            if len(p)>1 and p[1]=="*":
-                while p[0]==s[0] or p[0]==".":
-                    s=s[1:]
-                    if s=="":
-                        break
-                p=p[2:]
-            else:
-                if p[0]!=s[0] and p[0]!=".":
-                    return False
+        d=[]
+        for i in range(len(s)+1):
+            d.append([])
+            for j in range(len(p)+1):
+                if i==0:
+                    if j==0:d[i].append(True)
+                    elif p[j-1]=="*":d[i].append(d[i][j-2])
+                    else:d[i].append(False)
+                elif j==0:
+                    d[i].append(False)
+                elif s[i-1]==p[j-1] or p[j-1]==".":
+                    d[i].append(d[i-1][j-1])
+                elif p[j-1]=="*":
+                    if d[i][j-2]:d[i].append(d[i][j-2])
+                    elif s[i-1]==p[j-2] or p[j-2]==".":
+                        d[i].append(d[i-1][j])
+                    else:d[i].append(False)
                 else:
-                    p=p[1:]
-                    s=s[1:]
-        if len(s)>0:
-            return False
-        return True
+                    d[i].append(False)
+        return d[i][j]
