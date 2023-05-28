300105afc982ac80588bbaf481980df886eea762
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2023-05-28 20:46:16 +0700

    Create _38_count_and_say.py

---
+++ _38_count_and_say.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n<3:return "1"*n
+        a=self.countAndSay(n-1)
+        print(a)
+        s=""
+        i=1
+        c=int("1"+a[0])
+        while i<len(a):
+            if c%10 == int(a[i]):
+                c+=10
+            else:
+                s+=str(c)
+                c=int("1"+a[i])
+            i+=1
+        return s+str(c)
